be3fcabe8aa30eb598152bfa7c451cee51559d94
Saboor-Hakimi <44523544+Saboor-Hakimi@users.noreply.github.com>
2020-02-10 04:54:51 +0430

    Add files via upload

---
+++ customtheme/header.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <script
+  src="https://code.jquery.com/jquery-3.4.1.min.js"
+  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+  crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="
+  https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="wp-content/themes/customtheme/style.css">
+    <title>AACA</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top shadow">
+        <a class="navbar-brand"><img src="wp-content/themes/customtheme/images/images.png" alt="Brand" width="140px"></a>
+        <button class="navbar-toggler" data-target="#my-nav" data-toggle="collapse" aria-controls="my-nav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div id="my-nav" class="collapse navbar-collapse ml-5">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link active" href="#company">The Company</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#vision">Vision, Mission and Objectives </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#manage">Management Team</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#reg">Registrations and Associations </a>
+                </li>
+            </ul>
+            <ul class="navbar-right">
+                
+            <li class="nav-item nav-link">
+                    <a href="#" class="nav-link">
+                        <i class="fa fa-search" aria-hidden="true" style="font-size:2rem;"></i>
+                     </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
---
+++ customtheme/index.php
@@ -0,0 +1,94 @@
+<?php get_header(); ?>
+  <!-- Other Content here -->
+  <br><br><br>
+  <div class="container">
+
+  <h1 class="font-weight-bolder">
+    The Company
+  </h1>
+  <div class="info font-weight-bold">
+  
+<img src="wp-content/themes/customtheme/images/images.png" alt="company" height="100px" class="float-lg-right">
+    <br>
+    
+  Our leadership members come with up to two decades of <br>
+hands-on national and international experience across <br>
+multiple industries and in various areas of expertise.  <br>
+
+  </div>
+
+
+  <hr>
+  <br>
+  <p class="text-justify">
+  AACS Consulting is a leading professional services and management consulting company founded in early 2009 and headquartered in Kabul, Afghanistan. With combined experience of nearly four decades, a diverse team
+of national and international experts enables AACS Consulting to provide customized and quality professional services. These services are offered in areas of Audit and Tax Advisory, Business Development, Research and
+Survey, Monitoring and Evaluation, Capacity Building, and Operation Support.
+  </p>
+  <p class="text-justify">
+  For nearly a decade, AACS Consulting has helped a wide range of government, United Nations, international development, and business institutions maximize on opportunities and overcome business challenges in
+various areas and industries. Over the years, AACS Consulting has developed into a reputable and trusted brand in the professional services and management consulting industry, delivering more than 110+ projects,
+which has resulted in substantial growth and expansion of the firm in the Afghan market
+  </p>
+
+  <br>
+  <hr>
+
+  <br>
+  <h2 class="font-weight-bolder">
+    Awards and Recognition
+  </h2>
+
+    <br>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-2 bg-dark mr-1">sadf</div>
+        <div class="col-lg-2 bg-dark mr-1">sadf</div>
+        <div class="col-lg-2 bg-dark mr-1">asdfas <br><br> <br> <br> <br> <br> <br></div>
+        <div class="col-lg-2 bg-dark mr-1">asdsaf</div>
+        <div class="col-lg-2 bg-dark">asdfds</div>
+
+      </div>
+    </div>
+
+    <br>
+    <hr>
+    </div>
+    <div class="jumbotron">
+    <div class="container">
+    <h2 class="font-weight-bolder">
+    Our Mission and Values
+    </h2>
+    <br>
+    <p class="text-justify">
+    AACS Consulting’s mission is to design and deliver high impact solutions that visibly enhance growth, improve operations and bring tangible value for our clients. We partner with our clients in realizing their goals; we
+accomplish this through continually developing our knowledge, resources, skills and systems in a rewarding environment that encourages teamwork, innovation and excellence. At AACS Consulting, we focus on
+generating concrete results; not merely reports and numbers. We do much more. We care. We care about our clients and their business. Our philosophy is to ask ourselves what we would want if we were in the client’s
+place. 
+    </p>
+    <p class="text-justify">What sets us apart from the rest is that we consider our clients as partners, and thus, see their success as ours. We are committed to developing valuable working relationship with each client-partner and maintaining the
+highest professional and ethical standards. </p>
+    <p class="text-justify">To achieve this mission and for us to thrive further as a business, AACS Consulting must remain a great workplace for our greatest asset, our team. In doing so, we remain committed to continuously attract, retain and
+empower great talent by providing our team with rewarding career opportunities, fair benefits, and a great culture. </p>
+ <br>
+ <p class="text-justify font-weight-bolder">At AACS Consulting, we live by and are driven by these five core values:</p>
+ <br><br>
+
+  <div style="height: 150px; border: 1px solid black; border-radius: 0% 150px 150px 0;">
+  <span style="width: 70%;position: absolute; height: 150px; border: 1px solid black; border-radius: 0% 150px 150px 0;">  asdfds
+  </span>
+  <span style="width: 55%;position: absolute; height: 150px; border: 1px solid black; border-radius: 0% 150px 150px 0;">  asdfds
+  </span>
+  <span style="width: 40%;position: absolute; height: 150px; border: 1px solid black; border-radius: 0% 150px 150px 0;">  asdfds
+  </span>
+  <span style="width: 20%;position: absolute; height: 150px; border: 1px solid black; border-radius: 0% 150px 150px 0;">  asdfds
+  </span>
+  </div>
+  </div>
+
+
+</div>
+
+
+<?php get_footer(); ?>
