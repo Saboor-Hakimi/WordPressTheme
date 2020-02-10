a95650b4f50a2cd03835d44c8ef177b1ca4f1dc0
Saboor-Hakimi <44523544+Saboor-Hakimi@users.noreply.github.com>
2020-02-10 04:52:01 +0430

    Add files via upload

---
+++ customtheme/footer.php
@@ -0,0 +1,32 @@
+<footer>
+
+    <div class="container bg-primary mb-5 text-light">
+    <br><br><br><br>
+        <img src="wp-content/themes/customtheme/images/images.png" alt="" class="float-lg-right" width="45%">
+    <br><br><br><br><br><br><br><br><br><br><br><br>
+        <div>
+            <div class="card text-dark ml-5 shadow">
+                <div class="card-body">
+                    <h4>CAREERS</h4>
+                    <h2>Join Us</h2>
+                    <p>Join our diverse group of innovators—working to solve
+                        industries’ and organizations’ most challenging
+                        problems together.
+                    </p>
+                    <button class="btn hover" style="border-radius: 40px;border: 2px solid #007bff;">&nbsp;&nbsp; READ MORE &nbsp;&nbsp;</button>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+<?php
+
+    wp_footer();
+?>
+</footer>
+<br><br><br><br>
+</body>
+
+</html>
